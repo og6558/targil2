cba47e6cc40906c1282f7eedeb27684557e0c50e
og6558 <og6558@bs.amalnet.k12.il>
2022-01-19 22:54:25 +0200

    Initial commit

---
+++ app/src/main/java/com/example/targil2/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.targil2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn;
+    int mona = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.btn);
+    }
+
+    public void inbar2(View view) {
+        mona++;
+        btn.setText("This is a click number:" + mona);
+    }
+}
